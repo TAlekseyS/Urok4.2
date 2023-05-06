c4164efe5c1822da598e950517585518d7b62450
Aleksey_T <talekseys@mail.ru>
2023-05-06 14:23:49 +0300

    Gitignore

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
+        int cost = 11000;
+        int miles = service.calculate(cost);
+
+
+        System.out.println(miles);
+
+
+    }
+}
+
