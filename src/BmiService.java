b99b3309f85f9c203ff3b50e815b9031a235a762
Aleksey_T <talekseys@mail.ru>
2023-05-10 00:18:30 +0300

    Sdelal2 Again

--- src/BmiService.java
+++ src/BmiService.java
@@ -2,7 +2,7 @@ public class BmiService {
 
     public double calculate(double heightCm, double weightKg) {
 
-        double bmi = weightKg / ( ( heightCm / 100) * ( heightCm / 100 ) );
+        double bmi = weightKg / ((heightCm / 100) * (heightCm / 100));
         return bmi;
     }
 
